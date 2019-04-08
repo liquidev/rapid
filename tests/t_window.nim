e03f25ee8a1587660a942c656aeae6356fd41d0c
liquid600pgm <liquidekgaming@gmail.com>
2019-04-08 19:03:17 +0200

    Revert "Gfx rewrite"
    This reverts commit 7b2a7377935f41ce7ace2ab8882b3287f2370b1f.
    Apparently, everything was fine except I wasn't binding the VBO before setting its attributes.

--- tests/t_window.nim
+++ tests/t_window.nim
@@ -13,7 +13,11 @@ suite "windows":
 
     win.loop(
       proc (step: float) =
-        drawExample(gfx),
+        var ctx = gfx.ctx
+        ctx.clear(rgb(0, 0, 255))
+        ctx.begin()
+        ctx.tri((0.0, 0.5), (-0.5, -0.5), (0.5, -0.5))
+        ctx.draw(),
       proc (delta: float) =
         discard
     )
