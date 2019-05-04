c8d1b7192332fc2f00f8ef58ec6f249863e31b62
liquid600pgm <liquidekgaming@gmail.com>
2019-05-05 00:29:57 +0200

    Glyph rendering

--- src/rapid/data/fonts.nim
+++ src/rapid/data/fonts.nim
@@ -4,34 +4,75 @@
 # copyright (c) 2019, iLiquid
 #--
 
+import tables
+import unicode
+
 import ../lib/freetype
 
 import textures
 
 type
+  Glyph = ref object
+    texture: RTexture
+    bitmapLeft, bitmapTop, advanceX, advanceY: int
   RFont* = ref object
     handle*: FT_Face
-  RFontRenderMode* = enum
-    frPixel
-    frSmooth
-    frLCD
-    frLCDV
+    # TODO: optimize text rendering to use a single packed texture for all
+    #       characters
+    glyphs*: TableRef[Rune, Glyph]
+    conf*: FontConfig
   FontConfig* = object
-    path: string
-    height, width: int
-    mode: RFontRenderMode
+    texConf: RTextureConfig
   FreetypeError* = object of Exception
 
 var freetypeLib*: FT_Library
 
 proc newRFont*(file: string,
-                height: int, width = 0, renderMode = frSmooth): RFont =
+               height: Natural, width = 0.Natural,
+               textureConfig: RTextureConfig): RFont =
   once:
     let err = FT_Init_Freetype(addr freetypeLib).bool
-    if err:
-      raise newException(FreetypeError, "Could not initialize FreeType")
-  let err = FT_New_Face(freetypeLib, file, 0, addr result.handle)
+    doAssert not err, "Could not load FreeType"
+
+  new(result)
+  result.conf = FontConfig(
+    texConf: textureConfig
+  )
+  var err = FT_New_Face(freetypeLib, file, 0, addr result.handle)
   if err == FT_Err_Unknown_File_Format:
     raise newException(FreetypeError, "Unknown font format (" & file & ")")
   elif err.bool:
     raise newException(FreetypeError, "Could not load font " & file & "")
+  err = FT_Set_Pixel_Sizes(result.handle, width.FT_uint, height.FT_uint)
+  doAssert not err.bool, "Could not set font size"
+
+proc render(font: RFont, rune: Rune): Glyph =
+  let glyphIdx = FT_Get_Char_Index(font.handle, rune.FT_ulong)
+  var err = FT_Load_Char(font.handle, glyphIdx, FT_LOAD_DEFAULT)
+  doAssert not err.bool, "Could not load glyph '" & $rune & "'"
+
+  err = FT_Render_Glyph(font.handle.glyph, FT_RENDER_MODE_NORMAL)
+  doAssert not err.bool, "Failed to render glyph '" & $rune & "'"
+
+  let
+    glyph = font.handle.glyph
+    bitmap = glyph.bitmap
+    buffer = cast[UncheckedArray[uint8]](bitmap.buffer)
+  var glImg: seq[uint8]
+  # TODO: waste of memory, replace this with a shader-based way of rendering
+  for y in 0..<bitmap.rows:
+    for x in 0..<bitmap.width:
+      glImg.add([
+        255'u8, 255, 255,
+        buffer[x + y * bitmap.width]])
+  let tex = newRTexture(bitmap.width.int, bitmap.rows.int,
+                        glImg[0].unsafeAddr, font.conf.texConf)
+  result = Glyph(
+    texture: tex,
+    bitmapLeft: glyph.bitmapLeft, bitmapTop: glyph.bitmapTop,
+    advanceX: glyph.advance.x, advanceY: glyph.advance.y
+  )
+
+proc render*(font: RFont, glyph: string) =
+  let rune = glyph.runeAt(0)
+  font.glyphs[rune] = font.render(rune)
