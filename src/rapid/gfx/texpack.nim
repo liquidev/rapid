38731a5bc71622e9c3a83c5dee0f68bca16ade4e
liquid600pgm <liquidekgaming@gmail.com>
2019-10-11 18:15:51 +0200

    Texture packer modes Added a ``fast`` packing mode, which has O(1) insertion time but is much less efficient.

--- src/rapid/gfx/texpack.nim
+++ src/rapid/gfx/texpack.nim
@@ -18,11 +18,18 @@ import ../res/textures
 import ../world/aabb
 
 type
+  RTexturePackerMode* = enum
+    tpmEfficient
+    tpmFast
   RTexturePacker* = ref object
     texture*: RTexture
-    occupied: seq[RAABounds]
     fmt: RTexturePixelFormat
     x, y: int
+    case mode: RTexturePackerMode
+    of tpmEfficient:
+      occupied: seq[RAABounds]
+    of tpmFast:
+      lineMaxY: int
   RTextureRect* = tuple
     x, y, w, h: float
 
@@ -40,32 +47,47 @@ proc rawPlace(tp: RTexturePacker, x, y, w, h: int, data: pointer) =
   glTexSubImage2D(GL_TEXTURE_2D, 0, x.GLint, GLint(tp.texture.height - y - h),
                   w.GLsizei, h.GLsizei, tp.fmt.color, GL_UNSIGNED_BYTE, data)
 
+proc packEfficient(tp: RTexturePacker, image: RImage): RTextureRect =
+  var
+    x = tp.x
+    y = tp.y
+  while y <= tp.texture.height - image.height:
+    while x <= tp.texture.width - image.width:
+      block placeTex:
+        for area in tp.occupied:
+          if area.has(vec2(x.float, y.float)):
+            x = int area.x + area.width
+            break placeTex
+        if tp.areaFree(x, y, image.width, image.height):
+          tp.rawPlace(x, y, image.width, image.height, image.caddr)
+          tp.occupyArea(x, y, image.width, image.height)
+          let
+            hp = 1 / tp.texture.width / tp.texture.width.float / 2
+            vp = 1 / tp.texture.height / tp.texture.height.float / 2
+          return (x / tp.texture.width + hp, y / tp.texture.height + vp,
+                  image.width / tp.texture.width - hp * 2,
+                  image.height / tp.texture.height - vp * 2)
+        inc(x)
+    x = 0
+    inc(y)
+  tp.x = x
+  tp.y = y
+
+proc packFast(tp: RTexturePacker, image: RImage): RTextureRect =
+  if tp.x + image.width >= tp.texture.width:
+    tp.x = 0
+    tp.y = tp.lineMaxY
+  tp.rawPlace(tp.x, tp.y, image.width, image.height, image.caddr)
+  tp.x += image.width
+  if tp.lineMaxY < tp.y + image.height:
+    tp.lineMaxY = tp.y + image.height
+
 proc pack(tp: RTexturePacker, image: RImage): RTextureRect =
-  if image.width * image.height > 0:
-    var
-      x = tp.x
-      y = tp.y
-    while y <= tp.texture.height - image.height:
-      while x <= tp.texture.width - image.width:
-        block placeTex:
-          for area in tp.occupied:
-            if area.has(vec2(x.float, y.float)):
-              x = int area.x + area.width
-              break placeTex
-          if tp.areaFree(x, y, image.width, image.height):
-            tp.rawPlace(x, y, image.width, image.height, image.caddr)
-            tp.occupyArea(x, y, image.width, image.height)
-            let
-              hp = 1 / tp.texture.width / tp.texture.width.float / 2
-              vp = 1 / tp.texture.height / tp.texture.height.float / 2
-            return (x / tp.texture.width + hp, y / tp.texture.height + vp,
-                    image.width / tp.texture.width - hp * 2,
-                    image.height / tp.texture.height - vp * 2)
-          inc(x)
-      x = 0
-      inc(y)
-    tp.x = x
-    tp.y = y
+  if image.width > 0 and image.height > 0:
+    result =
+      case tp.mode
+      of tpmEfficient: tp.packEfficient(image)
+      of tpmFast: tp.packFast(image)
 
 proc place*(tp: RTexturePacker, image: RImage): RTextureRect =
   ## Place an image onto the texture packer's target.
@@ -86,9 +108,10 @@ proc place*(tp: RTexturePacker, images: openarray[RImage]): seq[RTextureRect] =
 
 proc newRTexturePacker*(width, height: Natural,
                         conf = DefaultTextureConfig,
-                        fmt = fmtRGBA8): RTexturePacker =
+                        fmt = fmtRGBA8, mode = tpmEfficient): RTexturePacker =
   ## Creates a new texture packer.
   result = RTexturePacker(
     texture: newRTexture(width, height, nil, conf, fmt),
-    fmt: fmt
+    fmt: fmt,
+    mode: mode
   )
